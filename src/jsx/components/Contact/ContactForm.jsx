432ffd1ec6070485ceca31347cbd5d0c22baacb8
Alex Ward <alex@volcaniccreations.com>
2016-08-09 14:40:09 +0100

    form structire added ready for react integration

--- src/jsx/components/Contact/ContactForm.jsx
+++ src/jsx/components/Contact/ContactForm.jsx
@@ -19,6 +19,18 @@ export default class ContactForm extends React.Component {
             </select>
             <span>for a project</span>
           </div>
+          <div className="input_container">
+            <label htmlFor="email">You can email me at:</label>
+            <input type="email" name="email" id="email" value="" placeholder="Enter your email address"/>
+          </div>
+          <div className="input_container">
+            <label htmlFor="tel">or call me on:</label>
+            <input type="tel" name="tel" id="tel" value="" placeholder="Enter your telephone number"/>
+          </div>
+          <div className="input_container">
+            <label htmlFor="more_info">Here is some more information:</label>
+            <textarea name="more_info" id="more_info" cols="30" rows="10" placeholder="more information about the project"></textarea>
+          </div>
         </fieldset>
       </form>
     )
