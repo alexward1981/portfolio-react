75684e9348c4dc0da08f6c481354bc090659daa2
Alex Foxleigh <alex@alexfoxleigh.com>
2018-05-16 12:35:53 +0100

    Fixed issue by bypassing built in mongoose pluralisation 'feature'

--- server/models/config.js
+++ server/models/config.js
@@ -1,12 +1,16 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
+// Cache the collection name
+var c = 'Config'
+
 // Config schema
 var ConfigSchema = new Schema({
   status: {
     type: String,
     required: true
   }
-});
-var Model = mongoose.model('Config', ConfigSchema);
-module.exports = Model;
+})
+// Export the model and specify the collection name to avoid pluralisation (hence the repeated 'c' variable)
+var Model = mongoose.model(c, ConfigSchema, c)
+module.exports = Model
