0b8b55ba1f27ca23fc4fe8ca44322ea11f626904
Alex Foxleigh <alex@alexfoxleigh.com>
2018-05-17 23:15:31 +0100

    Added mutations for skills -add -update -remove

--- server/graphql/types/social-networks.js
+++ server/graphql/types/social-networks.js
@@ -1,30 +1,35 @@
+var GraphQLObjectType = require('graphql').GraphQLObjectType
+var GraphQLBoolean = require('graphql').GraphQLBoolean
+var GraphQLString = require('graphql').GraphQLString
 
-var GraphQLObjectType = require('graphql').GraphQLObjectType;
-var GraphQLBoolean = require('graphql').GraphQLBoolean;
-var GraphQLString = require('graphql').GraphQLString;
+let dataShape = {
+  name: {
+    type: GraphQLString,
+    description: 'The name of the social network'
+  },
+  icon_class: {
+    type: GraphQLString,
+    description: 'If an icon is supposed to appear with the service, this is the class to show that icon. (e.g. `fa fa-facebook-official`)'
+  },
+  username: {
+    type: GraphQLString,
+    description: 'The username used on the service'
+  },
+  profile_url: {
+    type: GraphQLString,
+    description: 'The full profile URL (including username if applicable) of the service'
+  },
+  cta_text: {
+    type: GraphQLString,
+    description: 'A snippet of text to use as a call to action. (e.g. `Follow me on facebook`)'
+  },
+  active: {
+    type: GraphQLBoolean,
+    description: 'Should this service appear on the site? (Defaults to true)'
+  }
+}
 
 exports.socialNetworksType = new GraphQLObjectType({
   name: 'socialNetworks',
-  fields: function() {
-    return {
-      name: {
-        type: GraphQLString
-      },
-      icon_class: {
-        type: GraphQLString
-      },
-      username: {
-        type: GraphQLString
-      },
-      profile_url: {
-        type: GraphQLString
-      },
-      cta: {
-        type: GraphQLString
-      },
-      active: {
-        type: GraphQLBoolean
-      }
-    }
-  }
+  fields: () => dataShape
 })
