886e55211623dd075caa9995fed281cc95de6afd
Alex Foxleigh <alex@alexfoxleigh.com>
2018-05-17 19:16:28 +0100

    [WIP] working on mutations@

--- server/graphql/types/config.js
+++ server/graphql/types/config.js
@@ -1,5 +1,6 @@
 
 var GraphQLObjectType = require('graphql').GraphQLObjectType;
+var GraphQLInputObjectType = require('graphql').GraphQLInputObjectType;
 var GraphQLNonNull = require('graphql').GraphQLNonNull;
 var GraphQLBoolean = require('graphql').GraphQLBoolean;
 var GraphQLList = require('graphql').GraphQLList;
@@ -9,6 +10,27 @@ var GraphQLString = require('graphql').GraphQLString;
 // Config Type
 exports.configType = new GraphQLObjectType({
   name: 'config',
+  fields: function () {
+    return {
+      status: {
+        type: GraphQLString
+      },
+      available_from: {
+        type: GraphQLString
+      },
+      special_message: {
+        type: GraphQLString
+      },
+      CV_URL: {
+        type: GraphQLString
+      }
+    }
+  }
+});
+
+// Config Input Type
+exports.configInputType = new GraphQLInputObjectType({
+  name: 'configInputType',
   fields: function () {
     return {
       status: {
