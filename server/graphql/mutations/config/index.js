0e81099f2fb2e70c3c74bbb19f125a66b5860eb0
Alex Foxleigh <alex@alexfoxleigh.com>
2018-05-17 19:16:28 +0100

    [WIP] working on mutations@

--- server/graphql/mutations/config/index.js
+++ server/graphql/mutations/config/index.js
@@ -6,15 +6,17 @@ var ConfigModel = require('../../../models/config');
 // TODO: Still working on mutations
 exports.update = {
   type: ConfigType.configType,
-  resolve(root, args, params) {
+  description: 'Update a config item',
+  args: {
+    configUpdate: { type: ConfigType.configInputType }
+  },
+  resolve(root, { configUpdate }, params) {
     return ConfigModel.findOneAndUpdate(
       { 
-        $set: { 
-          status: params.status,
-          available_from: params.available_from,
-          special_message: params.special_message,
-          CV_URL: params.CV_URL,
-        } 
+        status: params.status,
+        available_from: params.available_from,
+        special_message: params.special_message,
+        CV_URL: params.CV_URL,
       },
       { new: true }
     )
